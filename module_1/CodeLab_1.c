9ba87539f75dae6677e795c719900232bee85ec6
フェノム (Phenom) <142201491+phenomu@users.noreply.github.com>
2023-09-25 18:27:53 +0700

    Create CodeLab_1.c

---
+++ module_1/CodeLab_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+	int a = 12;
+	int b = 28;
+	float c = 2.5;
+	float d = 8.9;
+	char e = 'R';
+
+	//(tambahkan komentar disini)
+	int sum = a + (b * c) - d;
+	printf("Bilangan a: %d\n", a);
+	printf("Bilangan b: %d\n", b);
+	printf("Bilangan c: %.2f\n", c);
+	printf("Bilangan d: %.2f\n", d);
+	printf("Karakter e: %c\n", e);
+	printf("Hasil: %f\n", sum);
+	return 0;
+}
