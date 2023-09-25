9db8fd5bc4a6ade210b16c299121ef05b62f99b7
フェノム (Phenom) <142201491+phenomu@users.noreply.github.com>
2023-09-26 00:21:44 +0700

    Update!

---
+++ module_1/Kegiatan_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+	
+int main(){
+	
+	int kal,pan,ork;
+
+	//Follow For More_^
+	printf("Masukan Nilai Kalkulus	: ");
+	scanf("%d", &kal);
+	printf("Masukan Nilai Pancasila	: ");
+	scanf("%d", &pan);
+	printf("Masukan Nilai Orkom	: ");
+	scanf("%d", &ork);
+	printf("\n\n");
+	printf("Nilai Kalkulus Kamu: %d\nNilai Pancasila Kamu: %d\nNilai Orkom Kamu: %d\n",kal, pan, ork);
+	
+	return 0;
+}
