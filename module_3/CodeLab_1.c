b1059ade91731886e634c801bd4d8252e08e55e4
フェノム (Phenom) <142201491+phenomu@users.noreply.github.com>
2023-09-28 07:44:11 +0000

    Update!

---
+++ module_3/CodeLab_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+	int nilai;
+
+    //Follow My Github For More_^
+	printf("Masukkan nilai Anda: ");
+	scanf("%d", &nilai);
+
+	if (nilai >= 0 && nilai <= 100) {
+		if (nilai >= 90) {
+			printf("Nilai Anda adalah A.\n");
+		}else if (nilai >= 80) {
+			printf("Nilai Anda adalah B.\n");
+		}else if (nilai >= 70) {
+			printf("Nilai Anda adalah C.\n");
+		}else if (nilai > 60) {
+			printf("Nilai Anda adalah D.\n");
+		}else {
+			printf("Nilai Anda adalah E.\n");
+		}
+		printf("Program Selesai");
+	}else{
+		printf("Input tidak valid. Nilai harus berada dalam rentang 0 hingga 100.\n");
+	}
+return 0;
+}
